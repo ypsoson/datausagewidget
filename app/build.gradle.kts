594d9ad41c939880386de1a0c9ca507835907c9d
Ypsoson <ypsoson@webyy.cz>
2026-09-03 15:52:55 +0200

    Improved accessibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.czczypsoson.datausagewidget"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
